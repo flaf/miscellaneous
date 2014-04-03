912e245e8d77806da97c1c4e848fa439caf6320b
root <root@wheezy-clean.crdp.ac-versailles.fr>
2014-04-03 19:43:23 +0200

    Update

--- radio-puppet-lab/icecast2/manifests/init.pp
+++ radio-puppet-lab/icecast2/manifests/init.pp
@@ -4,10 +4,29 @@ class icecast2 {
     ensure => present,
   }
 
+  file { '/etc/icecast2/icecast.xml.puppet':
+    ensure  => present,
+    owner   => 'icecast2',
+    group   => 'icecast',
+    mode    => 660,
+    content => template('icecast2/icecast.xml.puppet.erb'),
+    require => Package['icecast2'],
+    notify  => Service['icecast2'],
+  }
+
+  file { '/etc/default/icecast2':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 644,
+    content => template('icecast2/icecast.default.erb'),
+    require => Package['icecast2'],
+    notify  => Service['icecast2'],
+  }
+
   service { 'icecast2':
     hasrestart => true,
     ensure     => running,
-    require    => Package['icecast2'],
   }
 
 }
