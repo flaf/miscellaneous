b82016780ea39311165bfe37f4d2ea4bbde85bcf
flaf <flalfdivers@free.fr>
2014-04-10 17:54:41 +0200

    Update put_labconf

--- radio-puppet-lab/put_labconf.sh
+++ radio-puppet-lab/put_labconf.sh
@@ -3,7 +3,10 @@
 ROOT_DIR=$(cd $(dirname "$0"); pwd)
 PUPPET_DIR='/puppet/production'
 
+rm -f "$PUPPET_DIR/modules/icecast2"
 ln -s "$ROOT_DIR/icecast2" "$PUPPET_DIR/modules/icecast2"
+
+rm -f "$PUPPET_DIR/hieradata/fqdn/radio-puppet.athome.priv.yaml"
 ln -s "$ROOT_DIR/radio-puppet.athome.priv.yaml" "$PUPPET_DIR/hieradata/fqdn/radio-puppet.athome.priv.yaml"
 
 
