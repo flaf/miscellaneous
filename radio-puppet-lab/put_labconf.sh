dbd283a3e5f9f6c3de2785060cbd1f3d98c9e410
flaf <flalfdivers@free.fr>
2014-05-01 02:26:31 +0200

    Update

--- radio-puppet-lab/put_labconf.sh
+++ radio-puppet-lab/put_labconf.sh
@@ -3,13 +3,20 @@
 ROOT_DIR=$(cd $(dirname "$0"); pwd)
 PUPPET_DIR='/puppet/production'
 
-rm -f "$PUPPET_DIR/modules/icecast2"
+rm -rf "$PUPPET_DIR/modules/icecast2"
 ln -s "$ROOT_DIR/icecast2" "$PUPPET_DIR/modules/icecast2"
 
-rm -f "$PUPPET_DIR/modules/airtime"
+rm -rf "$PUPPET_DIR/modules/airtime"
 ln -s "$ROOT_DIR/airtime" "$PUPPET_DIR/modules/airtime"
 
 rm -f "$PUPPET_DIR/hieradata/fqdn/radio-puppet.athome.priv.yaml"
 ln -s "$ROOT_DIR/radio-puppet.athome.priv.yaml" "$PUPPET_DIR/hieradata/fqdn/radio-puppet.athome.priv.yaml"
 
+rm -rf "$PUPPET_DIR/modules/exim4"
+ln -s "$ROOT_DIR/../shinken-puppet-lab/exim4" "$PUPPET_DIR/modules/exim4"
+
+rm -f "$PUPPET_DIR/modules/repositories/manifests/sourcefabric.pp"
+ln -s "$ROOT_DIR/sourcefabric.pp" "$PUPPET_DIR/modules/repositories/manifests/sourcefabric.pp"
+
+echo "Don't forget to insert 'sourcefabric' entry in hieradata/default.yaml..."
 
