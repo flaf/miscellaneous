7047526593f140c05d86ec6873d0774c33a40b95
flaf <flalfdivers@free.fr>
2014-05-18 21:43:16 +0200

    Update

--- puppet/production/modules/common/manifests/init.pp
+++ puppet/production/modules/common/manifests/init.pp
@@ -8,6 +8,8 @@ class common {
                        'dnsutils',
                        'psmisc',
                        'gawk',
+                       'screen',
+                       'tcpdump',
                      ]
 
   package { $common_packages:
