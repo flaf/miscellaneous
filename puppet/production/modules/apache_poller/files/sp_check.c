506c971a829c58fae9f76bc9217d16450366e9c5
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-07-12 03:38:45 +0200

    Update

--- puppet/production/modules/apache_poller/files/sp_check.c
+++ puppet/production/modules/apache_poller/files/sp_check.c
@@ -1,8 +1,9 @@
-// Sur Debian Wheezy, faire :
-//
-//     sudo apt-get install libcurl4-openssl-dev libssl-dev
-//     gcc -lssl -lcurl -std=c99 -o curl-launcher.exe curl-launcher.c
-//
+/* With Debian Wheezy:
+ *
+ *    sudo apt-get install libcurl4-openssl-dev
+ *    gcc -lcurl -std=c99 -o curl-launcher.exe curl-launcher.c
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,43 +11,40 @@
 #include <ctype.h>
 #include <curl/curl.h>
 
-//#define MAX_BUF         65536
-#define MAX_BUF         25
+#define MAX_BUF         65536
 #define MAX_POST_LENGTH 4096
-
-#define OK       0
-#define WARNING  1
-#define CRITICAL 2
-#define UNKNOWN  3
-
-
-static char wr_buf[MAX_BUF+1] = {0};
+#define OK              0
+#define WARNING         1
+#define CRITICAL        2
+#define UNKNOWN         3
 
 
-// Write data callback function (called within the context of
-// curl_easy_perform).
+
+
+// Write data callback function (called within the context
+// of curl_easy_perform).
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+
+    // It is assumed that the type of userp is char*.
+    char *wr_buf = (char *)userp;
 
     static int wr_index = 0;
     int segsize = size * nmemb;
 
     // Check to see if this data exceeds the size of our buffer. If so,
-    // set the user-defined context value and return 0 to indicate a
-    // problem to curl.
+    // it's possible to return O to indicate a problem to curl.
+    // But here, we just stop the function without error (ie, we return
+    // segsize) and our buffer will be troncated.
     if (wr_index + segsize > MAX_BUF) {
-        *(int *)userp = 1;
-        printf("Sorry, the max output size of the plugin is exceeded.\n");
-        return 0;
+        memcpy((void*)&wr_buf[wr_index], buffer, (size_t)(MAX_BUF-wr_index));
+        return segsize;
     }
 
     // Copy the data from the curl buffer into our buffer.
-    memcpy((void *)&wr_buf[wr_index], buffer, (size_t)segsize);
+    memcpy((void*)&wr_buf[wr_index], buffer, (size_t)segsize);
 
     // Update the write index.
     wr_index += segsize;
-
-    // Null terminate the buffer.
-    wr_buf[wr_index] = 0;
 
     // Return the number of bytes received, indicating to curl that
     // all is okay.
@@ -83,7 +81,8 @@ int main(int argc, char *argv[]) {
     }
 
     if (!isPositiveInteger(argv[1])) {
-        printf("Sorry, bad syntax. The first argument must be a positive integer.\n");
+        printf("Sorry, bad syntax. The first argument must be a positive");
+        printf(" integer (it's timeout in seconds).\n");
         return UNKNOWN;
     }
 
@@ -109,7 +108,7 @@ int main(int argc, char *argv[]) {
     for (i = 3; i < argc; i++) {
 
         if (token_num > 999) {
-            printf("Sorry, the limit number (999) of POST variables is exceeded.");
+            printf("Sorry, the limit number (999) of POST variables is exceeded.\n");
             curl_easy_cleanup(curl);
             return UNKNOWN;
         }
@@ -142,32 +141,52 @@ int main(int argc, char *argv[]) {
     post[strlen(post)-1] = 0;
     //printf("C: POST [%s]\n", post);
 
+    char wr_buf[MAX_BUF+1] = {0};
+
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
-    // Tell curl that we'll receive data to the function write_data, and
-    // also provide it with a context pointer for our error return.
-    int wr_error = 0;
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&wr_error);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
+
+    // Tell curl that we'll receive data to the function write_data
+    // which will write the data in wr_buf.
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)wr_buf);
 
     // Allow curl to perform the action.
     CURLcode ret;
     ret = curl_easy_perform(curl);
 
     if (ret) {
-        printf("exit value of curl %d (write_error = %d)\n", ret, wr_error);
+        printf("Sorry, exit value of curl %d.\n", ret);
+        curl_easy_cleanup(curl);
         return UNKNOWN;
     }
 
-    // Emit the page if curl indicates that no errors occurred.
-    //if (ret == 0) {
-    //    printf("%s", wr_buf);
-    //}
-
     curl_easy_cleanup(curl);
-    return 0;
+
+    /*
+    printf("----------------------------------\n");
+    printf("%s", wr_buf);
+    printf("----------------------------------\n");
+    */
+
+    int return_value;
+    if (!strncmp(wr_buf, "0\n", 2)) {
+        return_value = OK;
+    } else if (!strncmp(wr_buf, "1\n", 2)) {
+        return_value = WARNING;
+    } else if (!strncmp(wr_buf, "2\n", 2)) {
+        return_value = CRITICAL;
+    } else if (!strncmp(wr_buf, "3\n", 2)) {
+        return_value = UNKNOWN;
+    } else {
+        printf("Unexpected output of the plugin, return value not");
+        printf(" displayed or not in {0, 1, 2, 3}.\n");
+        return UNKNOWN;
+    }
+
+    printf("%s", wr_buf+2);
+    return return_value;
 
 }
 
