50b5624726b469ae6618512eb6a535cd40dac7f2
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-07-22 01:43:31 +0200

    Update

--- puppet/production/modules/apache_poller/files/sp_check.c
+++ puppet/production/modules/apache_poller/files/sp_check.c
@@ -3,14 +3,6 @@
  *    sudo apt-get install libcurl4-openssl-dev
  *    gcc -std=c99 -Wextra -Wall -O2 -o sp_check sp_check.c -lcurl
  *
- *    TODO:
- *          char wr_buf[MAX_BUF_IN_BYTES + 1] = { 0 }; --> char wr_buf[MAX_BUF_IN_BYTES];
- *          Pas d'initialisation (car ça coûte en ressource). Mettre un \0
- *          à la fin juste après une memcpy dans la fonction write_data.
- *          (attention au changement d'indice).
- *
- *          Comme le suggère Xavier Roche, faire de *userp une structure.
- *
  */
 
 #include <stdio.h>
@@ -26,213 +18,269 @@
 #define CRITICAL          2
 #define UNKNOWN           3
 
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
-
-int isPositiveInteger(const char s[]);
-
 typedef struct Buffer Buffer;
-struct Buffer {
-  char buf[MAX_BUF_IN_BYTES];
+
+struct Buffer
+{
+  char wr_buf[MAX_BUF_IN_BYTES];
   size_t index;
+  size_t capa;
 };
 
+void init_buffer ( Buffer * buffer )
+{
+  buffer->wr_buf[0] = '\0';     // an empty string by default.
+  buffer->index = 0;
+  buffer->capa = MAX_BUF_IN_BYTES;
+}
+
+int is_filled ( Buffer * buffer )
+{
+  if ( buffer->index >= buffer->capa - 1 )
+  {
+    // Because (buffer->wr_buf)[buffer->cap - 1] must contain '\0'
+    // and can be used.
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+// If this function does not return segsize, it will signal an error 
+// condition to the library. This will cause the transfer to get aborted
+// and the libcurl function used will return CURLE_WRITE_ERROR.
+size_t write_data ( void *buffer, size_t size, size_t nmemb, void *userp )
+{
+  // The size (in bytes) of the received buffer.
+  int segsize = size * nmemb;
+
+  // In this function, userp refers to a Buffer structure.
+  Buffer *userbuf = userp;
+
+  if ( userbuf->index >= userbuf->capa - 1 )
+  {
+    // userbuf is already filled.
+    return 0;
+  }
+
+  // Get truncated_segsize.
+  int truncated_segsize = segsize;
+
+  if ( userbuf->index + segsize >= userbuf->capa )
+  {
+    truncated_segsize = userbuf->capa - 1 - userbuf->index;
+  }
+
+  if ( truncated_segsize > 0 )
+  {
+    memcpy ( &( userbuf->wr_buf )[userbuf->index], buffer,
+             truncated_segsize );
+    userbuf->index += truncated_segsize;
+    // After each change, userbuf->wr_buf must be a valid string.
+    ( userbuf->wr_buf )[userbuf->index] = '\0';
+    return truncated_segsize;
+  }
+  else
+  {
+    userbuf->index = userbuf->capa - 1;
+    return 0;
+  }
+}
+
+int isPositiveInteger ( const char s[] )
+{
+  if ( s == NULL || *s == '\0' )
+  {
+    return 0;
+  }
+
+  size_t i;
+
+  for ( i = 0; s[i] != '\0'; i++ )
+  {
+    // Test if all the characters are digits.
+    if ( !isdigit ( s[i] ) )
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
 
 
 
-int main(int argc, char *argv[]) {
 
-  if (argc < 4) {
-    printf("Sorry, bad syntax. You must apply at least 3 arguments.\n");
-    printf("%s <timeout> <url> <args>...\n", argv[0]);
+int main ( int argc, char *argv[] )
+{
+  if ( argc < 4 )
+  {
+    printf ( "Sorry, bad syntax. You must apply at least 3 arguments.\n" );
+    printf ( "%s <timeout> <url> <args>...\n", argv[0] );
     return UNKNOWN;
   }
 
-  if (!isPositiveInteger(argv[1])) {
-    printf("Sorry, bad syntax. The first argument must be a positive");
-    printf(" integer (it's a timeout in seconds).\n");
+  if ( !isPositiveInteger ( argv[1] ) )
+  {
+    printf ( "Sorry, bad syntax. The first argument must be a positive" );
+    printf ( " integer (it is a timeout in seconds).\n" );
     return UNKNOWN;
   }
 
-  const int timeout = atoi(argv[1]);
+  const int timeout = atoi ( argv[1] );
   const char *const url = argv[2];
 
   // First step, init curl.
   CURL *curl;
-  curl = curl_easy_init();
 
-  if (!curl) {
-    printf("Sorry, couldn't init curl.\n");
+  curl = curl_easy_init (  );
+
+  if ( !curl )
+  {
+    printf ( "Sorry, could not init curl.\n" );
     return UNKNOWN;
   }
 
   // Construction of the post variable, a string with this form:
   //      token1=<urlencoded data1>&token2=<urlencoded data2>&...
   char post[MAX_POST_IN_BYTES];
+
   post[0] = '\0';
   int token_num = 1;
   char *urlencoded_str = NULL;
   int i = 0;
 
-  for (i = 3; i < argc; i++) {
-
-    if (token_num > 999) {
+  for ( i = 3; i < argc; i++ )
+  {
+    if ( token_num > 999 )
+    {
       printf
-        ("Sorry, the limit number (999) of POST variables is exceeded.\n");
-      curl_easy_cleanup(curl);
+        ( "Sorry, the limit number (999) of POST variables is exceeded.\n" );
+      curl_easy_cleanup ( curl );
       return UNKNOWN;
     }
 
-    //printf("C: token%d: [%s]\n", token_num, argv[i]);
+    //printf("token%d: [%s]\n", token_num, argv[i]);
 
-    urlencoded_str = curl_easy_escape(curl, argv[i], 0);
+    urlencoded_str = curl_easy_escape ( curl, argv[i], 0 );
 
     // 10 is the max length of the string "token<num>=&".
     // The maximum is reached with "token999=&".
-    int temp_size = 10 + strlen(urlencoded_str) + 1;
+    int temp_size = 10 + strlen ( urlencoded_str ) + 1;
     char temp[temp_size];
-    sprintf(temp, "token%d=%s&", token_num, urlencoded_str);
 
-    if (strlen(post) + strlen(temp) + 1 < MAX_POST_IN_BYTES) {
-      strcat(post, temp);
+    sprintf ( temp, "token%d=%s&", token_num, urlencoded_str );
+
+    if ( strlen ( post ) + strlen ( temp ) + 1 < MAX_POST_IN_BYTES )
+    {
+      strcat ( post, temp );
     }
-    else {
-      printf("Sorry, the max POST size is exceeded.\n");
-      curl_easy_cleanup(curl);
+    else
+    {
+      printf ( "Sorry, the max POST size is exceeded.\n" );
+      curl_easy_cleanup ( curl );
       return UNKNOWN;
     }
 
-    curl_free(urlencoded_str);
+    curl_free ( urlencoded_str );
     token_num++;
-
   }
 
   // Remove the last character "&".
-  post[strlen(post) - 1] = 0;
-  //printf("C: POST [%s]\n", post);
+  post[strlen ( post ) - 1] = 0;
 
-  char wr_buf[MAX_BUF_IN_BYTES + 1] = { 0 };
+  //printf("POST [%s]\n", post);
 
-  curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
+  curl_easy_setopt ( curl, CURLOPT_URL, url );
+  curl_easy_setopt ( curl, CURLOPT_TIMEOUT, timeout );
+  curl_easy_setopt ( curl, CURLOPT_POSTFIELDS, post );
 
-  // Tell curl that we'll receive data to the function write_data
-  // which will write the data in wr_buf.
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) wr_buf);
+  // Tell curl that we will receive data to the function write_data
+  // which will write the data in "buf".
+  Buffer buf;
+
+  init_buffer ( &buf );
+  curl_easy_setopt ( curl, CURLOPT_WRITEFUNCTION, write_data );
+  curl_easy_setopt ( curl, CURLOPT_WRITEDATA, &buf );
 
   // Allow curl to perform the action.
   CURLcode ret;
-  ret = curl_easy_perform(curl);
 
-  if (ret) {
-    curl_easy_cleanup(curl);
-    printf("Sorry, exit value of curl is %d.", ret);
+  ret = curl_easy_perform ( curl );
 
-    switch (ret) {
+  if ( ret )
+  {
+    if ( ret != CURLE_WRITE_ERROR || !is_filled ( &buf ) )
+    {
+      curl_easy_cleanup ( curl );
+      printf ( "Sorry, exit value of curl_easy_perform is %d.", ret );
 
-    case CURLE_COULDNT_RESOLVE_HOST:
-      printf(" Could not resolve the host address.\n");
-      break;
+      switch ( ret )
+      {
 
-    case CURLE_OPERATION_TIMEDOUT:
-      printf(" Operation timeout.\n");
-      break;
+      case CURLE_COULDNT_RESOLVE_HOST:
+        printf ( " Could not resolve the host address.\n" );
+        break;
 
-    default:
-      printf("\n");
-      break;
+      case CURLE_OPERATION_TIMEDOUT:
+        printf ( " Operation timeout.\n" );
+        break;
 
+      default:
+        printf ( "\n" );
+        break;
+
+      }
+
+      return UNKNOWN;
     }
-
-    return UNKNOWN;
   }
 
-  curl_easy_cleanup(curl);
+  curl_easy_cleanup ( curl );
 
   /*
      printf("----------------------------------\n");
-     printf("%s", wr_buf);
+     printf("%s", buf.wr_buf);
      printf("----------------------------------\n");
    */
 
   int return_value;
-  if (!strncmp(wr_buf, "0\n", 2)) {
+
+  if ( !strncmp ( buf.wr_buf, "0\n", 2 ) )
+  {
     return_value = OK;
   }
-  else if (!strncmp(wr_buf, "1\n", 2)) {
+  else if ( !strncmp ( buf.wr_buf, "1\n", 2 ) )
+  {
     return_value = WARNING;
   }
-  else if (!strncmp(wr_buf, "2\n", 2)) {
+  else if ( !strncmp ( buf.wr_buf, "2\n", 2 ) )
+  {
     return_value = CRITICAL;
   }
-  else if (!strncmp(wr_buf, "3\n", 2)) {
+  else if ( !strncmp ( buf.wr_buf, "3\n", 2 ) )
+  {
     return_value = UNKNOWN;
   }
-  else {
-    printf("Unexpected output of the plugin, return value not");
-    printf(" displayed or not in {0, 1, 2, 3}.\n");
+  else
+  {
+    printf ( "Unexpected output of the plugin, return value not" );
+    printf ( " displayed or not in {0, 1, 2, 3}.\n" );
     return UNKNOWN;
   }
 
-  printf("%s", wr_buf + 2);
+  char *output = buf.wr_buf + 2;
+  size_t len = strlen ( output );
+
+  if ( len > 0 && output[len - 1] == '\n' )
+  {
+    printf ( "%s", output );
+  }
+  else
+  {
+    printf ( "%s\n", output );
+  }
+
   return return_value;
-
-}
-
-
-
-
-// Write data callback function (called within the context
-// of curl_easy_perform).
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
-
-  // We want to keep the value of wr_index for the next call
-  // this function. So, wr_index is a static variable.
-  static int wr_index = 0;
-
-  // The size (in bytes) of the received buffer.
-  int segsize = size * nmemb;
-
-  // In this function, userp refers to a simple array of bytes.
-  char *wr_buf = userp;
-
-  // Check to see if this data exceeds the size of our buffer. If so,
-  // it's possible to return O to indicate a problem to curl.
-  // But here, we just stop the function without error (ie, we return
-  // segsize) and our buffer will be troncated.
-  if (wr_index + segsize > MAX_BUF_IN_BYTES) {
-    if (MAX_BUF_IN_BYTES - wr_index > 0) {
-      memcpy(&wr_buf[wr_index], buffer, MAX_BUF_IN_BYTES - wr_index);
-    }
-    wr_index = MAX_BUF_IN_BYTES + 1; // wr_buf will be not written anymore.
-    return segsize;
-  }
-
-  // Copy the data from the curl buffer into our buffer.
-  memcpy(&wr_buf[wr_index], buffer, segsize);
-
-  // Update the write index.
-  wr_index += segsize;
-
-  // Return the number of bytes received, indicating to curl that
-  // all is okay.
-  return segsize;
-}
-
-int isPositiveInteger(const char s[]) {
-
-  if (s == NULL || *s == '\0') {
-    return 0;
-  }
-
-  size_t i;
-  for (i = 0; s[i] != '\0'; i++) {
-    // Test if all the characters are digits.
-    if (! isdigit(s[i])) {
-      return 0;
-    }
-  }
-
-  return 1;
 }
