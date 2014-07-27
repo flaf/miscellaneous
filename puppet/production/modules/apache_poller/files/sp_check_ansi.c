81b836483fb4f163dfb3edabe21dd44b9993d2d9
Francois Lafont <flafdivers@free.fr>
2014-07-27 10:01:48 +0200

    Update

--- puppet/production/modules/apache_poller/files/sp_check_ansi.c
+++ puppet/production/modules/apache_poller/files/sp_check_ansi.c
@@ -75,9 +75,9 @@ static int is_filled ( const Buffer * const buffer )
 
 static int is_positive_integer ( const char s[] )
 {
-  assert ( s != NULL );
-
   size_t i;
+
+  assert ( s != NULL );
 
   for ( i = 0; s[i] != '\0'; i++ )
   {
@@ -97,7 +97,7 @@ static int is_positive_integer ( const char s[] )
  *       token0=<urlencoded data1>&token1=<urlencoded data2>&...
  */
 static int write_post ( char *post, const unsigned int post_size,
-                        const char *const data[], const unsigned int data_num,
+                        char *data[], const unsigned int data_num,
                         CURL * const curl )
 {
   unsigned int d_index = 0;     /* data index */
@@ -154,6 +154,7 @@ static size_t write_data ( void *buffer, size_t size, size_t nmemb,
 {
   /* The size (in bytes) of the received buffer. */
   const int segsize = size * nmemb;
+  int truncated_segsize = segsize;
 
   /* In this function, userp refers to a Buffer structure. */
   Buffer *const userbuf = userp;
@@ -163,9 +164,7 @@ static size_t write_data ( void *buffer, size_t size, size_t nmemb,
     return 0;
   }
 
-  /* Get truncated_segsize. */
-  int truncated_segsize = segsize;
-
+  /* Update truncated_segsize. */
   if ( userbuf->index + segsize >= userbuf->capa )
   {
     truncated_segsize = userbuf->capa - 1 - userbuf->index;
@@ -190,8 +189,17 @@ static size_t write_data ( void *buffer, size_t size, size_t nmemb,
 
 
 
-int main ( const int argc, const char *const argv[] )
+int main ( const int argc, char *argv[] )
 {
+  int timeout;
+  char *url = NULL;
+  char post[MAX_POST_IN_BYTES];
+  Buffer buf;
+  CURL *curl = NULL;
+  CURLcode ret;
+  unsigned int return_value;
+  char *output = NULL;
+  size_t len;
 
   if ( argc < 4 )
   {
@@ -209,12 +217,10 @@ int main ( const int argc, const char *const argv[] )
     return UNKNOWN;
   }
 
-  const int timeout = atoi ( argv[1] );
-  const char *const url = argv[2];
+  timeout = atoi ( argv[1] );
+  url = argv[2];
 
   /* First step, init curl. */
-  CURL *curl;
-
   curl = curl_easy_init (  );
 
   if ( !curl )
@@ -224,8 +230,6 @@ int main ( const int argc, const char *const argv[] )
   }
 
   /* Construction of the post string. */
-  char post[MAX_POST_IN_BYTES];
-
   if ( !write_post ( post, MAX_POST_IN_BYTES, argv + 3, argc - 3, curl ) )
   {
     curl_easy_cleanup ( curl );
@@ -240,13 +244,9 @@ int main ( const int argc, const char *const argv[] )
    * Tell curl that we will receive data to the function write_data
    * which will write the data in "buf".
    */
-  Buffer buf;
-
   init_buffer ( &buf );
   curl_easy_setopt ( curl, CURLOPT_WRITEFUNCTION, write_data );
   curl_easy_setopt ( curl, CURLOPT_WRITEDATA, &buf );
-
-  CURLcode ret;
 
   /* Allow curl to perform the action. */
   ret = curl_easy_perform ( curl );
@@ -283,8 +283,6 @@ int main ( const int argc, const char *const argv[] )
    * printf("----------------------------------\n");
    */
 
-  int return_value;
-
   if ( !strncmp ( buf.wr_buf, "0\n", 2 ) )
   {
     return_value = OK;
@@ -308,8 +306,8 @@ int main ( const int argc, const char *const argv[] )
     return UNKNOWN;
   }
 
-  char *output = buf.wr_buf + 2;
-  size_t len = strlen ( output );
+  output = buf.wr_buf + 2;
+  len = strlen ( output );
 
   if ( len > 0 && output[len - 1] == '\n' )
   {
