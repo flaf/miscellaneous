3fdccccac788740782378a841a05c66c32acfe02
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-08-05 16:48:41 +0200

    Update

---
+++ puppet/production/modules/ceph_lab/manifests/init.pp
@@ -0,0 +1,18 @@
+ceph_lab {
+
+  # Python 2.7 must be installed.
+  package { 'python':
+    ensure => present,
+  }
+
+  file { '/usr/local/sbin/Ceph_ready':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => 755,
+    source => 'puppet:///modules/ceph_lab/Ceph_ready',
+  }
+
+}
+
+
