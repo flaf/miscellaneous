867d35a83eab4a82a52d7d0569eb5f73d3c2a892
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-10-14 19:00:11 +0200

    Update

--- puppet/production/modules/profiles/manifests/ceph.pp
+++ puppet/production/modules/profiles/manifests/ceph.pp
@@ -2,22 +2,21 @@ class profiles::ceph {
 
   include apt
 
-  #apt::key { 'ceph':
-  #  key        => '17ED316D',
-  #  key_source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
-  #}
-
   $ceph_release = "firefly"
 
   apt::source { 'ceph':
-    location          => "http://ceph.com/debian-${ceph_release}/",
-    release           => $::lsbdistcodename,
-    repos             => 'main',
-    key               => '17ED316D',
-    include_src       => false,
+    location    => "http://ceph.com/debian-${ceph_release}/",
+    release     => $::lsbdistcodename,
+    repos       => 'main',
+    key         => '17ED316D',
+    include_src => false,
   }
 
-
+  if ! defined(Package['ceph']) {
+    package { 'ceph':
+      ensure  => present,
+      require => Apt::Source['ceph'],
+  }
 
 }
 
