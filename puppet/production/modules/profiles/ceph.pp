45a3280a59bd4cbc9fd295994e63661502e76d27
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-10-14 18:19:36 +0200

    Update

---
+++ puppet/production/modules/profiles/ceph.pp
@@ -0,0 +1,14 @@
+class profiles::ceph {
+
+  include apt
+
+  apt::key { 'jenkins':
+    key        => '17ED316D',
+    key_source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+  }
+
+
+
+
+}
+
