aeb42af3dae13663794ee7c36fcd45a3fd0b16b5
flaf <flalfdivers@free.fr>
2014-10-29 06:31:26 +0100

    Update

--- puppet/production/modules/gammu_smsd/manifests/web_service.pp
+++ puppet/production/modules/gammu_smsd/manifests/web_service.pp
@@ -1,4 +1,73 @@
-class web_service {
+# Class: gammu_smsd::web_service
+#
+# This is an internal class.
+#
+class gammu_smsd::web_service {
+
+  require '::gammu_smsd'
+
+  if ! defined(Package['libapache2-mod-perl2']) {
+    package { 'libapache2-mod-perl2':
+      ensure => present,
+    }
+  }
+
+  file { '/var/spool/gammu/outbox':
+    owner   => 'gammu',
+    group   => 'www-data',
+    mode    => '770',
+    require => Package['libapache2-mod-perl2'],
+  }
+
+  file { '/usr/lib/cgi-bin/sendsms.pl':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0755,
+    source  => "puppet:///modules/${module_name}/sendsms.pl",
+    require => Package['libapache2-mod-perl2'],
+    notify  => Service['apache2'],
+  }
+
+  exec { 'disable default vhost':
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+    user    => 'root',
+    group   => 'root',
+    command => 'a2dissite default',
+    # Exec only if there is already the symlink.
+    onlyif  => 'test -L /etc/apache2/sites-enabled/000-default',
+    require => Package['libapache2-mod-perl2'],
+    notify  => Service['apache2'],
+  }
+
+  file { 'sms vhost':
+    path    => '/etc/apache2/sites-available/sms_vhost',
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 644,
+    source  => "puppet:///modules/${module_name}/sms_vhost",
+    require => Package['libapache2-mod-perl2'],
+    notify  => Service['apache2'],
+  }
+
+  exec { 'enable sms vhost':
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+    user    => 'root',
+    group   => 'root',
+    command => 'a2ensite sms_vhost',
+    # Exec unless there is already the symlink.
+    unless  => 'test -L /etc/apache2/sites-enabled/sms_vhost',
+    require => File['sms vhost'],
+    notify  => Service['apache2'],
+  }
+
+  service { 'apache2':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['libapache2-mod-perl2'],
+  }
 
 }
 
--- puppet/production/modules/profiles/manifests/smssender.pp
+++ puppet/production/modules/profiles/manifests/smssender.pp
@@ -2,6 +2,7 @@ class profiles::smssender {
 
   class {'::gammu_smsd':
     phones_to_test => [ '0676553219' ],
+    web_service    => true,
   }
 
 }
