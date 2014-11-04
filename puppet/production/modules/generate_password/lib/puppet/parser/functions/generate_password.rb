1b34cfce6c730fb4be8ecd584c15ef8dc07553db
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-11-04 01:31:19 +0100

    Update

--- puppet/production/modules/generate_password/lib/puppet/parser/functions/generate_password.rb
+++ puppet/production/modules/generate_password/lib/puppet/parser/functions/generate_password.rb
@@ -1,54 +1,4 @@
-# Here is a copy of the docstring but without the \"
-# to be more readable directly in the source code.
-#
-#  Usage
-#  -----
-#    * In a yaml file (in hieradata):
-#          password: '__pwd__{<syntax explained below>}'
-#    * In a manifest:
-#          password = hiera('password')
-#       or password = generate_password(hiera('password'))
-#    * In a template:
-#          <%= scope.function_generate_password([@password]) %>
-#       or <%= @password %>
-#
-#  Possible syntax in a yaml file
-#  ------------------------------
-#      key: '__pwd__{}'
-#      key: '__pwd__{ "pwd" => "master_password", "salt" => ["$fqdn"], "nice" => false, "max_length" => 0, "case" => "" }'
-#      key: '__pwd__{ "pwd" => "other_password", "salt" => ["$datacenter"], "nice" => true, "case" => "lower" }'
-#      key: '__pwd__{ "salt" => ["$datacenter", "snmp"], "case" => "upper" }'
-#
-#  Each key and value must be surrounded by single or double quote
-#  even $fqdn, $datacenter, etc. variables.
-#  The only exception to this rules are:
-#    - the value of "max_length" which is an interger;
-#    - the value of "nice" which is an boolean (false or true).
-#
-#  Parameters in yaml file
-#  -----------------------
-#      "pwd"
-#          gives the key name of a password in extdata
-#          Default value: "master_password"
-#
-#      "salt"
-#          an array of strings which gives the salt.
-#          Default value: ["$fqdn"]
-#
-#      "nice"
-#          if equal to true, the password is nice, ie ~ /[a-zA-Z0-9]/.
-#          Default value: false
-#
-#      "max_length"
-#          truncates the password to N characters if the length
-#          of the password is greater than N.
-#          Default value: 0 (ie no truncation)
-#
-#      "case"
-#          Force the case of the password. Possible value are "lower",
-#          "upper" or "" (empty string).
-#          Default value: "" (do not change the password)
-
+# Copyright: 2014 Francois Lafont <francois.lafont@ac-versailles.fr>
 
 module Puppet::Parser::Functions
   newfunction(:generate_password, :type => :rvalue, :doc => "
@@ -57,8 +7,8 @@ module Puppet::Parser::Functions
     * In a yaml file (in hieradata):
           password: '__pwd__{<syntax explained below>}'
     * In a manifest:
-          password = hiera('password')
-       or password = generate_password(hiera('password'))
+          $password = hiera('password')
+       or $password = generate_password(hiera('password'))
     * In a template:
           <%= scope.function_generate_password([@password]) %>
        or <%= @password %>
@@ -70,7 +20,7 @@ module Puppet::Parser::Functions
       key: '__pwd__{ \"pwd\" => \"other_password\", \"salt\" => [\"$datacenter\"], \"nice\" => true, \"case\" => \"lower\" }'
       key: '__pwd__{ \"salt\" => [\"$datacenter\", \"snmp\"], \"case\" => \"upper\" }'
 
-  Each key and value must be surrounded by single or double quote
+  Each key and value must be surrounded by single or double quotes
   even $fqdn, $datacenter, etc. variables.
   The only exception to this rules are:
     - the value of \"max_length\" which is an interger;
@@ -79,7 +29,7 @@ module Puppet::Parser::Functions
   Parameters in yaml file
   -----------------------
       \"pwd\"
-          gives the key name of a password in extdata
+          gives the key name of a password in hiera
           Default value: \"master_password\"
 
       \"salt\"
@@ -101,8 +51,8 @@ module Puppet::Parser::Functions
           Default value: \"\" (do not change the password)
   ") do |args|
 
-    # Need to extlookup puppet function.
-    Puppet::Parser::Functions.function('extlookup')
+    # Need to "hiera" puppet function.
+    Puppet::Parser::Functions.function('hiera')
     require 'digest/md5'
 
     authorized_args = ['pwd', 'salt', 'max_length', 'nice', 'case']
@@ -134,8 +84,8 @@ module Puppet::Parser::Functions
         args_hash['case'] = ''
       end
 
-      # Get the "source" password with extlookup.
-      source_pwd = function_extlookup([args_hash['pwd']])
+      # Get the "source" password with hiera.
+      source_pwd = function_hiera([args_hash['pwd']])
 
       # The password will be generated from "clear" variable
       # which is the concatenation of the pwd and the salt.
@@ -187,5 +137,4 @@ module Puppet::Parser::Functions
 
   end
 end
-
 
