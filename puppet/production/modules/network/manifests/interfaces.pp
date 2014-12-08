ad2224c631340cef650866acf9f08111e851e1db
flaf <flalfdivers@free.fr>
2014-12-08 08:14:53 +0100

    Update

--- puppet/production/modules/network/manifests/interfaces.pp
+++ puppet/production/modules/network/manifests/interfaces.pp
@@ -40,10 +40,97 @@
 #               'method'     => 'static',
 #               'address'    => '172.31.20.6',
 #               'netmask'    => '255.255.0.0',
+#               'dns-search  => [ '@domain', 'toto.com', ],
+#             },
+#   'eth1' => {
+#               'macaddress' => '08:00:27:bc:cf:04',
+#               'method'     => 'dhcp',
+#               'comment'    => 'Blabla',
 #             },
 #  }
 #
+# For each interface, the "method" property is mandatory.
 # This parameter is mandatory and has no default value.
+# If the method is "static" the "address" and "netmask"
+# properties are mandatory. The value of a property can
+# be an array of strings. In this case, the value is
+# updated to value.join(' '). If a value is a string
+# with this form '@xxxx', it's automatically replaced by
+# the @xxxx fact.
+#
+# There are some properties, the meta options, which are
+# just puts in comments. Here is the list of meta options :
+#
+#  meta_options = [
+#                  'macaddress',
+#                  'network_name',
+#                  'vlan_name',
+#                  'vlan_id',
+#                  'cidr_address',
+#                  'comment',
+#                 ]
+#
+#
+# == Useful public functions
+#
+# *complete_ifaces_hash($ifaces, $inventoried_networks)*
+#
+# The function returns an updated version of $ifaces with
+# the informations inside $inventoried_networks which must
+# contain the "cidr_address". Here is an example:
+#
+#  $interfaces = {
+#   'eth0' => {
+#               'macaddress'     => '08:00:27:bc:cf:03',
+#               'method'         => 'static',
+#               'address'        => '172.31.20.6/16',
+#               'gateway'        => '<default>',
+#               'dns-nameservers => '<default>',
+#             },
+#  }
+#
+#  $inventoried_networks = {
+#    'private' => {
+#                   'cidr_address'    => '172.31.0.0/16',
+#                   'gateway'         => '172.31.0.1',
+#                   'dns-nameservers' => [ '172.31.0.2', '172.31.0.3', ],
+#                   'vlan_id'         => '0'
+#                   'ntp_servers      => [ '172.31.0.1', ]
+#                 }
+#    'public'  => {
+#                   'cidr_address'    => '192.168.0.0/24',
+#                   'gateway'         => '192.168.0.1',
+#                   'dns-nameservers' => [ '192.168.0.1', ],
+#                   'vlan_id'         => '1'
+#                   'ntp_servers      => [ '192.168.0.1', ]
+#                 }
+#
+#  $new = complete_ifaces_hash($ifaces, $inventoried_networks)
+#
+# In this example, the function use the CIDR address of eth0
+# to find the matching network. Then, each value equal to
+# '<default>' will be replaced by the value in the matching
+# network. Note: with an interface with a CIDR address, the
+# "netmask", "network" and "broadcast" properties will be
+# automatically appended. It's possible to don't use a CIDR
+# address for an interface, but if you want to help the
+# function to find a matching network, you must use the
+# "network_name" property in the interface ('public' or
+# 'private' in the example above)..
+#
+# *get_network_name($ipaddr, $netmask, $inventoried_networks)*
+#
+# Returns the name of the network which matches with the
+# $ipaddr and $netmask. Here is an example:
+#
+#  # Same value as the example above.
+#  $inventoried_networks = ...
+#
+#  $network_name = get_network_name($ipaddr, $netmask, $inventoried_networks)
+#
+#  # Now, we can retrieve the list of the ntp servers
+#  # in the current network.
+#  $ntp_servers = $inventoried_networks[$network_name]
 #
 #
 class network::interfaces (
