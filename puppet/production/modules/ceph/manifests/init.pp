69cd40199058ff03ebe207dd60fe877370c94310
flaf <flalfdivers@free.fr>
2014-12-09 17:24:16 +0100

    Update

--- puppet/production/modules/ceph/manifests/init.pp
+++ puppet/production/modules/ceph/manifests/init.pp
@@ -1,10 +1,22 @@
 class ceph (
-  $cluster_name = 'ceph'
+  $cluster_name          = 'ceph',
+  $osd_journal_size      = 1024,
+  $osd_pool_default_size = 2,
+  $fsid,
 ) {
 
   validate_string($cluster_name)
 
   require '::ceph::packages'
+
+
+  file { "/etc/ceph/${cluster_name}.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('ceph/ceph.conf.erb'),
+  }
 
 }
 
