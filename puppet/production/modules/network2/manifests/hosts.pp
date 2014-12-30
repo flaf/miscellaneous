e7bb9520b256cf81ee6d9f08f49a7023e5dd01dd
flaf <flalfdivers@free.fr>
2014-12-30 05:39:30 +0100

    Update

---
+++ puppet/production/modules/network2/manifests/hosts.pp
@@ -0,0 +1,32 @@
+class network::hosts (
+) {
+
+  case $::lsbdistcodename {
+    wheezy: {}
+    trusty: {}
+    default: {
+      fail("Class ${title} has never been tested on ${::lsbdistcodename}.")
+    }
+  }
+
+  file { '/etc/hosts.puppet.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    purge   => true,
+    force   => true,
+  }
+
+  file {'/etc/hosts.puppet.d/README':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Directory managed by Puppet, don't touch it.\n",
+  }
+
+}
+
+
