309e9cb78d153282d9ace3032fb9a8e1c35c5036
flaf <flalfdivers@free.fr>
2015-03-02 17:07:52 +0100

    Update

--- puppet/production/modules/ceph/manifests/cluster/packages.pp
+++ puppet/production/modules/ceph/manifests/cluster/packages.pp
@@ -5,6 +5,11 @@ class ceph::cluster::packages {
 
   private("Sorry, ${title} is a private class.")
 
+  # With Trusty, it's better to use a 3.16 kernel.
+  if $::lsbdistcodename == 'trusty' {
+    ensure_packages(['linux-image-generic-lts-utopic'], { ensure => present, })
+  }
+
   $packages = [
                 'ceph',
                 'ceph-mds',
