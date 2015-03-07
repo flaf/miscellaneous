293c2e586b107a8aa179ea2621dcb508d887f9ae
flaf <flalfdivers@free.fr>
2015-03-07 02:33:25 +0100

    Update

--- puppet/production/modules/ceph/manifests/client/packages.pp
+++ puppet/production/modules/ceph/manifests/client/packages.pp
@@ -6,8 +6,9 @@ class ceph::client::packages {
   private("Sorry, ${title} is a private class.")
 
   $packages = [
-                'ceph-common',
-                'ceph-fs-common',
+                'ceph-common',    # To use Rados block devices.
+                'ceph-fs-common', # To mount cephfs via the kernel.
+                'ceph-fuse',      # To mount cephfs via Fuse.
               ]
 
   ensure_packages($packages, { ensure => present, })
