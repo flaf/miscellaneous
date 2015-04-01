83431b24c73cf70d8fdbe2efc2c0ccb6e45124fb
flaf <flalfdivers@free.fr>
2015-04-01 05:28:30 +0200

    Update

--- puppet/production/modules/profiles/manifests/puppet/puppetmaster.pp
+++ puppet/production/modules/profiles/manifests/puppet/puppetmaster.pp
@@ -11,6 +11,7 @@ class profiles::puppet::puppetmaster {
   class { '::puppetmaster':
     server               => $server,
     ca_server            => $ca_server,
+    environment_timeout  => '0s',
     #puppetdb_user => 'joe',
     #admin_email   => 'sysadmin@domain.tld',
     hiera_git_repository => 'git@github.com:flaf/test-hiera.git',
