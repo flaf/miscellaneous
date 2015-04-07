57356edf5094f37c124b29c77bcd620075c1141e
François Lafont <flafdivers@free.fr>
2015-04-07 02:20:33 +0200

    Tue Apr  7 02:20:33 CEST 2015: Update

--- puppet/production/modules/mcollective/manifests/middleware.pp
+++ puppet/production/modules/mcollective/manifests/middleware.pp
@@ -2,6 +2,24 @@ class mcollective::middleware {
 
   ensure_packages( [ 'rabbitmq-server' ], { ensure => present, })
 
+  file { '/etc/rabbitmq/rabbitmq.config':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('mcollective/rabbitmq.config.erb'),
+    require => Package['rabbitmq-server'],
+    before  => Service['rabbitmq'],
+    notify  => Service['rabbitmq'],
+  }
+
+  service { 'rabbitmq':
+    name       => 'rabbitmq-server',
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+
 }
 
 
