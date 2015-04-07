466fe476f0f9f1e6c0d93a3dc89396e47044c497
François Lafont <flafdivers@free.fr>
2015-04-07 02:59:05 +0200

    Tue Apr  7 02:59:05 CEST 2015: Update

--- puppet/production/modules/mcollective/manifests/middleware.pp
+++ puppet/production/modules/mcollective/manifests/middleware.pp
@@ -1,6 +1,10 @@
 class mcollective::middleware {
 
-  ensure_packages( [ 'rabbitmq-server' ], { ensure => present, })
+  $packages = [ 'rabbitmq-server', 'python', ]
+  $cmd_cli  = "/var/lib/rabbitmq/mnesia/rabbit@*-plugins-expand/\
+rabbitmq_management-*/priv/www/cli/rabbitmqadmin"
+
+  ensure_packages($packages, { ensure => present, })
 
   file { '/etc/rabbitmq/rabbitmq.config':
     ensure  => present,
@@ -29,6 +33,23 @@ class mcollective::middleware {
     ensure     => running,
     hasstatus  => true,
     hasrestart => true,
+  }
+
+  exec { 'install-cli-mgt':
+    command => "cp \"${cmd_cli}\" /usr/local/sbin/",
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+    user    => 'root',
+    group   => 'root',
+    unless  => 'test -f /usr/local/sbin/rabbitmqadmin',
+    require  => Service['rabbitmq'],
+  }
+
+  file { '/usr/local/sbin/rabbitmqadmin':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0754',
+    require => Exec['install-cli-mgt'],
   }
 
 }
