d9c7966ffa81ff1b903a3649b9084936dda0e8de
François Lafont <flafdivers@free.fr>
2015-05-05 05:33:22 +0200

    Tue May  5 05:33:22 CEST 2015: Update

---
+++ puppet/production/modules/mcollective/manifests/server.pp
@@ -0,0 +1,30 @@
+class mcollective::server (
+) {
+
+  $packages = [
+               'ruby-stomp',
+               'mcollective',
+              ]
+
+  ensure_packages($packages, { ensure => present, })
+
+  file { '/etc/mcollective/server.cfg':
+    ensure  => present,
+    #owner   => 'root',
+    #group   => 'root',
+    #mode    => '0644',
+    content => template('mcollective/server.cfg.erb'),
+    require => Package['mcollective'],
+    before  => Service['mcollective'],
+    notify  => Service['mcollective'],
+  }
+
+  service { 'mcollective':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+
+}
+
+
