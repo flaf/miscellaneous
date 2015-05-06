f183196357ee08b380b3d355b2d9553eff3c82b8
François Lafont <flafdivers@free.fr>
2015-05-06 05:57:43 +0200

    Wed May  6 05:57:43 CEST 2015: Update

--- puppet/production/modules/mcollective/manifests/server.pp
+++ puppet/production/modules/mcollective/manifests/server.pp
@@ -1,4 +1,6 @@
 class mcollective::server (
+  $server_private_key,
+  $server_public_key,
 ) {
 
   $packages = [
@@ -8,16 +10,37 @@ class mcollective::server (
 
   ensure_packages($packages, { ensure => present, })
 
-  #file { '/etc/mcollective/server.cfg':
-  #  ensure  => present,
-  #  #owner   => 'root',
-  #  #group   => 'root',
-  #  #mode    => '0644',
-  #  content => template('mcollective/server.cfg.erb'),
+  $server_private_key_file = '/etc/mcollective/ssl/server-private.pem'
+  $server_public_key_file  = '/etc/mcollective/ssl/server-public.pem'
+
+  file { $server_private_key_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => $server_private_key,
+    require => Package['mcollective'],
+  }
+
+  file { $server_public_key_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => $server_public_key,
+    require => Package['mcollective'],
+  }
+
+  file { '/etc/mcollective/server.cfg':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('mcollective/server.cfg.erb'),
   #  require => Package['mcollective'],
   #  before  => Service['mcollective'],
   #  notify  => Service['mcollective'],
-  #}
+  }
 
   #service { 'mcollective':
   #  ensure     => running,
