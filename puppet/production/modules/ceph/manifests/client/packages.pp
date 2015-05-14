bad5cf3c9e80244c39455a444e53e8b7a46c8031
François Lafont <flafdivers@free.fr>
2015-05-14 17:10:59 +0200

    Thu May 14 17:10:58 CEST 2015: Update

--- puppet/production/modules/ceph/manifests/client/packages.pp
+++ puppet/production/modules/ceph/manifests/client/packages.pp
@@ -6,6 +6,10 @@ class ceph::client::packages {
   private("Sorry, ${title} is a private class.")
 
   $packages = [
+                'ceph',           # Seems to be surer to install ceph. For
+                                  # instance, cinder can run this command
+                                  # "ceph mon dump" which needs this package
+                                  # (especially the ceph_argparse module).
                 'ceph-common',    # To use Rados block devices.
                 'ceph-fs-common', # To mount cephfs via the kernel.
                 'ceph-fuse',      # To mount cephfs via Fuse.
