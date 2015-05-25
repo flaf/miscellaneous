016b25f40745f3c28463bef89d5b79c637adba58
François Lafont <flafdivers@free.fr>
2015-05-25 06:31:51 +0200

    Mon May 25 06:31:51 CEST 2015: Update

---
+++ puppet4-mod/locale/README.md
@@ -0,0 +1,19 @@
+# Module description
+
+This module just sets the default locale of the system.
+
+# Typical usage
+
+The `locale` class has only one parameter:
+
+```puppet
+class { 'locale':
+  default_locale => 'fr_FR.utf8',
+}
+```
+
+The `default_locale` parameter accepts only two values (two
+strings): `fr_FR.utf8` or `en_US.UTF-8`. The default value
+of this parameter is `en_US.UTF-8`.
+
+
---
+++ puppet4-mod/locale/manifests/init.pp
@@ -0,0 +1,31 @@
+class locale (
+  Enum['en_US.UTF-8', 'fr_FR.utf8'] $default_locale = 'en_US.UTF-8',
+) {
+
+  is_supported_distrib(['trusty'], $title)
+
+  file { '/etc/default/locale':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "LANG=${default_locale}\n",
+  }
+
+  # In fact, this is the opposite. The command below
+  # updates the default locale and write in the
+  # '/etc/default/locale' file. But, in this case,
+  # the file is a good way to know if the default
+  # locale is set as we want.
+  exec { 'update-default-locale':
+    path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => "update-locale LANG='${default_locale}'",
+    user        => 'root',
+    group       => 'root',
+    refreshonly => true,
+    require     => File['/etc/default/locale'],
+  }
+
+}
+
+
