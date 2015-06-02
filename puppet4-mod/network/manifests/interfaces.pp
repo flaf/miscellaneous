5ba07c78a0229502318a3d819c630ad9fc3bac02
François Lafont <flafdivers@free.fr>
2015-06-02 13:23:06 +0200

    Tue Jun  2 13:23:06 CEST 2015: Update

--- puppet4-mod/network/manifests/interfaces.pp
+++ puppet4-mod/network/manifests/interfaces.pp
@@ -3,8 +3,12 @@ class network::interfaces (
   Hash[ String[1],
         Hash[
               String[1],
-              Variant[ String[1], Array[String[1], 1] ],
-              1
+              Variant[
+                      String[1],
+                      Array[String[1], 1],
+                      Hash[String[1], String[1]]
+                     ],
+              2
             ],
         1
       ] $interfaces,
