3f5a7f097b9bf2746b605e7a78f23f750f52a56c
François Lafont <flafdivers@free.fr>
2015-06-02 17:43:58 +0200

    Tue Jun  2 17:43:58 CEST 2015: Update

--- puppet4-mod/network/manifests/interfaces.pp
+++ puppet4-mod/network/manifests/interfaces.pp
@@ -20,14 +20,14 @@ class network::interfaces (
   $interfaces.each |$interface, $settings| {
 
     unless has_key($settings, 'method') {
-      fail(regsubst(@("END"), '\n', ' ', 'G')
+      fail(regsubst(@("END"), '\n', ' ', 'G'))
       The interface ${interface} must have a 'method' key,
       this is not the case currently.
       |- END
     }
 
     unless is_string($settings['method']) {
-      fail(regsubst(@("END"), '\n', ' ', 'G')
+      fail(regsubst(@("END"), '\n', ' ', 'G'))
       The 'method' key of interface ${interface} must be a non
       empty string, this is not the case currently.
       |- END
