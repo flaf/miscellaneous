ac093d64249cca0442ac7034fbd1fa765d45be21
François Lafont <flafdivers@free.fr>
2015-06-02 02:05:53 +0200

    Tue Jun  2 02:05:53 CEST 2015: Update

---
+++ puppet4-mod/network/manifests/interfaces.pp
@@ -0,0 +1,17 @@
+class network::interfaces (
+  Boolean $restart_network = false,
+  Hash[ String[1],
+        Hash[
+              String[1],
+              Variant[ String[1], Array[String[1], 1] ],
+              1
+            ],
+        1
+      ] $interfaces,
+) {
+
+  is_supported_distrib(['trusty'], $title)
+
+}
+
+
