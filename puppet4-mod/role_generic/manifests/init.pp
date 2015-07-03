2037ffcd7d0c901caa0d83f423858f2a77dc6fa7
François Lafont <flafdivers@free.fr>
2015-07-03 17:42:12 +0200

    Fri Jul  3 17:42:12 CEST 2015: Update

--- puppet4-mod/role_generic/manifests/init.pp
+++ puppet4-mod/role_generic/manifests/init.pp
@@ -2,10 +2,6 @@ class role_generic {
 
   include '::basic_ssh::server'
   include '::basic_ssh::client'
-  #include '::profile_locale'
-  #include '::profile_timezone'
-  #include '::keyboard'
-  #include '::keyboard'
 
 }
 
--- puppet4/production/manifests/site.pp
+++ puppet4/production/manifests/site.pp
@@ -9,47 +9,12 @@ Stage['basis'] -> Stage['network']
 # We assume that the $::role variable is already defined
 # by the ENC and must be a non empty string.
 if $::role =~ String[1] {
-  include $::role
+  include "${::role}"
 } else {
-  fail('Sorry, no `$role` global variable is not defined for this node.')
+  fail(regsubst(@(END), '\n', ' ', 'G'))
+    Sorry, the node must have a `$role` global variable defined
+    and it must be a non empty string.
+    |- END
 }
-
-#class {'network::interfaces':
-#  interfaces => {
-#                  'eth1' => {
-#                              'macaddress' => 'ccc',
-#                              'method'     => 'static',
-#                              'comment'    => 'blabla',
-#                              'options' => { 'zbkey1' => 'val1', 'aaakey2' => 'val2' },
-#                            },
-#                  'eth0' => {
-#                              'macaddress' => 'aaa',
-#                              'method'     => 'static',
-#                              'comment'    => 'blabla',
-#                              'options' => { 'bkey1' => 'val1', 'aaakey2' => 'val2' },
-#                            },
-#                }
-#}
-#
-#
-#class { '::test':
-#  param1 => 'titi1',
-#}
-
-#include '::test'
-
-
-
-#$iface = 'eth0'
-#notify { 'heu':
-#
-#  message => getvar("::ipaddress_${iface}"),
-#
-#}
-
-#::homemade::is_supported_distrib([ 'trusty', 'jessie' ], 'trusty', 'site.pp')
-
-
-#::homemade::is_supp_distrib([ 'trusTy', 'jessie' ], ${title})
 
 
