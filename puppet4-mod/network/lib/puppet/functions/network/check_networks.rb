717cdb2427845873936d3c294af1086b17eb37c5
François Lafont <flafdivers@free.fr>
2015-07-07 21:19:39 +0200

    Tue Jul  7 21:19:39 CEST 2015: Update

--- puppet4-mod/network/lib/puppet/functions/network/check_networks.rb
+++ puppet4-mod/network/lib/puppet/functions/network/check_networks.rb
@@ -19,7 +19,7 @@ Puppet::Functions.create_function(:'network::check_networks') do
         # Check if each mandatory key is present.
         unless a_network.has_key?(key_name) and a_network[key_name].is_a?(key_type)
           msg_mandatory_keys = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
-          |#{function_name}(): the `#{netname}` network hash is not valid
+          |#{function_name}(): the `#{netname}` network hash is not valid.
           |The key `#{key_name}` must exist and its value must have the
           |`#{key_type.to_s}` type.
           EOS
@@ -35,7 +35,7 @@ Puppet::Functions.create_function(:'network::check_networks') do
           if a_network[k].empty?
             msg_empty_key = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
             |#{function_name}(): the `#{netname}` network hash is not valid
-            |because the value of the key `#{key_name}` is empty.
+            |because the value of the key `#{k}` is empty.
             EOS
             raise(Puppet::ParseError, msg_empty_key)
           end
--- puppet4/production/manifests/site.pp
+++ puppet4/production/manifests/site.pp
@@ -18,29 +18,3 @@ if $::role =~ String[1] {
 }
 
 
-#$a = ::network::get_matching_network('10.0.2.5/4')
-#$b = $::servername
-#$c = 'eeee'.::homemade::ljust(10, ' ')
-#$d = $::facts['networking']['interfaces']['eth0']['mac']
-##notify { 'Test': message => "[${a}]" }
-#notify { 'Test1': message => "[${b}]" }
-#notify { 'Test2': message => "[${c}]" }
-#notify { 'Test3': message => "[${d}]" }
-
-#['trusty', 'jessie'].::homemade::is_supported_distrib($title)
-
-
-#$vv = { 1 => 'a', 2 => 'b' }
-#$v =  {3 => 'c', 1 => 'ZZZZZZZZ'} +$vv
-##$v = $vv
-#notify { 'Test': message => "[${v}]" }
-
-$i = {
-  'eth0' => { 'method'     => 'dhcp',
-              'macaddress' => 'rrr',
-              'options'    => { "1" => '2', 'address' => '34' },
-   }
-}
-
-::network::check_interfaces($i)
-
