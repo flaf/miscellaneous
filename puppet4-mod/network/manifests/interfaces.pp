c27eb6ba1ab4ad5727e8b89f1f8c367e665f7e1b
lafont <francois.lafont@crdp.ac-versailles.fr>
2015-07-07 19:23:41 +0200

    Tue Jul  7 19:23:41 CEST 2015: Update

--- puppet4-mod/network/manifests/interfaces.pp
+++ puppet4-mod/network/manifests/interfaces.pp
@@ -1,29 +1,23 @@
 class network::interfaces (
-  Boolean $restart_network = false,
-  Hash[ String[1],
-        Hash[
-              String[1],
-              Variant[
-                      String[1],
-                      Hash[String[1], String[1], 1]
-                     ],
-              1
-            ],
-        1
-      ] $interfaces,
+  Boolean $restart_network,
+  Hash[String[1], Hash[String[1], Data, 1], 1] $interfaces,
   Array[String[1], 1] $supported_distributions,
 ) {
 
   ::homemade::is_supported_distrib($supported_distributions, $title)
+  ::network::check_interfaces($interfaces)
 
-  # Allowed keys in an interface hash.
-  $allowed_keys = [
-                   'method',
-                   'options',
-                   'network-name',
-                   'comment',
-                   'macaddress',
-                  ]
+  # (*)
+  # Trusty uses "resolvconf" by default and it's not
+  # recommended to remove "resolvconf" in Trusty (if you do
+  # that, you will remove the "ubuntu-minimal" package that
+  # is not recommended).
+  $packages = [
+               'resolvconf', # (*)
+               'vlan',       # To have vlan features.
+               'ifenslave',  # To have bonding features.
+              ]
+  ensure_packages($packages, { ensure => present, })
 
   $restart_network_cmd = @(END)
     ifdown --all
@@ -44,66 +38,6 @@ class network::interfaces (
     ifup --all
     sleep 0.25
     | END
-
-  # Check the $interfaces variable.
-  $interfaces.each |$interface, $settings| {
-
-    # The 'method' key is mandatory.
-    unless has_key($settings, 'method') {
-      fail(regsubst(@("END"), '\n', ' ', 'G'))
-      The interface '${interface}' must have a 'method' key,
-      this is not the case currently.
-      |- END
-    }
-
-    $settings.each |$param, $value| {
-
-      # Check if the keys of the interface are allowed.
-      unless member($allowed_keys, $param) {
-        fail(regsubst(@("END"), '\n', ' ', 'G'))
-        The interface '${interface}' has a '${param}' key
-        which is not allowed.
-        |- END
-      }
-
-      # Check the type of each value.
-      case $param {
-
-        'options': {
-          # The value of 'options' key must be a hash.
-          unless is_hash($value) {
-            fail(regsubst(@("END"), '\n', ' ', 'G'))
-            The value of the '${param}' key of interface '${interface}'
-            must be a non empty hash, this is not the case currently.
-            |- END
-          }
-        }
-
-        default: {
-          # The value of the other keys must be a string.
-          unless is_string($value) {
-            fail(regsubst(@("END"), '\n', ' ', 'G'))
-            The value of the '${param}' key of interface '${interface}'
-            must be a non empty string, this is not the case currently.
-            |- END
-          }
-        }
-
-      }
-    }
-  }
-
-  # (*)
-  # Trusty uses "resolvconf" by default and it's not
-  # recommended to remove "resolvconf" in Trusty (if you do
-  # that, you will remove the "ubuntu-minimal" package that
-  # is not recommended).
-  $packages = [
-               'resolvconf', # (*)
-               'vlan',       # To have vlan features.
-               'ifenslave',  # To have bonding features.
-              ]
-  ensure_packages($packages, { ensure => present, })
 
   file { '/etc/network/interfaces.puppet':
     ensure  => present,
