429bc68aa44b1b1b2c9dd254e2d6f5d51e83b7ec
lafont <francois.lafont@crdp.ac-versailles.fr>
2015-07-09 17:25:05 +0200

    Thu Jul  9 17:25:05 CEST 2015: Update

--- puppet4-mod/network/lib/puppet/functions/network/get_matching_network.rb
+++ puppet4-mod/network/lib/puppet/functions/network/get_matching_network.rb
@@ -2,13 +2,10 @@ Puppet::Functions.create_function(:'network::get_matching_network') do
 
   dispatch :get_matching_network do
     required_param 'Hash[String[1], Data, 1]', :an_interface
-    required_param 'Hash[String[1], Hash[String[1], Data, 1] , 1]', :networks
+    required_param 'Array[Hash[String[1], Data, 3], 1]', :networks
   end
 
   def get_matching_network(an_interface, networks)
-
-    call_function('::network::check_interface', an_interface)
-
 
     call_function('::network::check_interface', an_interface)
 
