96096a5b8410885febaa17b91e0884ec9f69dbcd
Francois Lafont <flafdivers@free.fr>
2015-07-15 03:59:19 +0200

    Wed Jul 15 03:59:19 CEST 2015: Update

--- puppet4-mod/network/lib/ruby/interface.rb
+++ puppet4-mod/network/lib/ruby/interface.rb
@@ -4,19 +4,26 @@ class Interface
 
   def initialize(conf)
 
-    @mandatory_keys = [
-                       'name',
-                       'method',
-                      ]
-    @allowed_keys   = {
-                       'name'         => String,
-                       'method'       => String,
-                       'options'      => Hash,
-                       'network-name' => String,
-                       'comment'      => String,
-                       'macaddress'   => String,
-                      }
-    @conf           = conf
+    @mandatory_keys   = [
+                         'name',
+                         'method',
+                        ]
+    @allowed_keys     = {
+                         'name'         => String,
+                         'method'       => String,
+                         'options'      => Hash,
+                         'network-name' => String,
+                         'comment'      => String,
+                         'macaddress'   => String,
+                        }
+    @conf               = conf
+    @has_address_option = false # will be updated (or not) below.
+
+    # Instance variables defined below:
+    #
+    #   - @ip_address
+    #   - @ip_network_address
+    #   - @name
 
     # Check if the mandatory keys are presents.
     @mandatory_keys.each do |key|
@@ -29,7 +36,7 @@ class Interface
       end
     end
 
-    # Some checkings...
+    # Some basic checkings...
     @conf.each do |key_name, key_value|
 
       # Check if each key of @conf is an allowed key.
@@ -66,11 +73,10 @@ class Interface
 
       if key_name == 'options'
 
-        options = @conf['options']
-        options.each do |k, v|
+        # Check if @conf['options'] is a hash of strings for
+        # the keys and the values.
+        @conf['options'].each do |k, v|
 
-          # Check if @conf['options'] is a hash of strings for
-          # the keys and the values.
           unless k.is_a?(String) and v.is_a?(String) and \
                  (not k.empty?) and (not v.empty?)
             msg_options_error = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
@@ -81,65 +87,200 @@ class Interface
             raise(Exception, msg_options_error)
           end
 
-          # Check the address.
           if k == 'address'
-
-            # TODO: il faut définir @ip_address et @ip_network_address
-            if v =~ Regexp.new('/[0-9]+$')
-              # This is a CIDR address.
-            else
-              # This is an IP address but not a CIDR address.
-            end
-
-            begin
-              @ip_address = IPAddr.new(v)
-            rescue ArgumentError
-              msg_bad_address = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
-                |The interface `#{@conf.to_s}` is not valid because
-                |the `address` option doesn't contain a valid IP
-                |address or a CIDR address.
-                EOS
-              raise(Exception, msg_bad_address)
-            end
-          else
-            # The instance variable will always exist
-            # but will be equal to nil if not defined.
-            @ip_address = nil
+            @has_address_option = true
           end
 
-        end # End of the loop on the keys of @conf['options'].
+        end
 
       end # End of the handle of the specific 'options' key.
 
-    end # End of the loop on the keys of @conf.
+    end # End of the loop on the keys of @conf and the basic checkings.
 
-    # Now, we are sure that the interface has a name.
-    @name = @conf['name']
+
+    # Check the address and define the:
+    #
+    #   - @ip_address
+    #   - @ip_network_address
+    #
+    # variable instances.
+    if @has_address_option
+
+      address_str = @conf['options']['address']
+      if address_str =~ Regexp.new('/[0-9]+$')
+
+        # This is a CIDR address.
+        ip_address_str        = address_str.split('/')[0]
+        begin
+          @ip_address         = IPAddr.new(ip_address_str)
+          @ip_network_address = IPAddr.new(address_str)
+        rescue ArgumentError
+          msg_bad_address = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+            |The interface `#{@conf.to_s}` is not valid because
+            |the `address` option doesn't contain a valid CIDR
+            |address.
+            EOS
+          raise(Exception, msg_bad_address)
+        end
+
+      else
+
+        # This is an IP address but not a CIDR address.
+        # The `netmask` option must exist.
+        unless @conf['options'].has_key?('netmask')
+          msg_no_netmask = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+            |The interface `#{@conf.to_s}` is not valid because
+            |there is an `address` option as an IP address (not
+            |as a CIDR address) but there is no `netmask` option.
+            EOS
+          raise(Exception, msg_no_netmask)
+        end
+
+        netmask = @conf['options']['netmask']
+        begin
+          @ip_address         = IPAddr.new(address_str)
+          @ip_network_address = IPAddr.new(address_str + '/' + netmask)
+        rescue ArgumentError
+          msg_bad_address = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+            |The interface `#{@conf.to_s}` is not valid because
+            |the `address` and/or the `netmask` options doesn't
+            |contain a valid address.
+            EOS
+          raise(Exception, msg_bad_address)
+        end
+
+      end
+
+    else
+
+      # The instance variables will always exist  but will be equal
+      # to nil if not defined.
+      @ip_address         = nil
+      @ip_network_address = nil
+
+    end # End of the handle if the interface has an `address` option.
+
+    # Now, we are sure that the interface has the `name`
+    # and `method` keys.
+    @name   = @conf['name']
+    @method = @conf['method']
 
   end
 
 
   def is_matching_network(network)
 
-    # TODO: tout reste à faire...
+    # false by default and we want to update this value
+    # to true only when it's matching.
+    result = false
+
     if @conf.has_key?('network-name')
 
-      if @conf['network-name'] == network.instance_variable_get(:@name)
-        if @ip_address.nil?
-          return true
-        else
-          
+      if @conf['network-name'] == network.get_name
+        if not @ip_network_address.nil?
+          # The interface has an IP address.
+          # We have to check that the address matches with the
+          # network address.
+          unless @ip_network_address.eql?(network.get_ip_address)
+            msg_pb_address = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+              |The interface `#{@conf.to_s}` should match with the
+              |`#{network.get_name}` network but the address
+              |doesn't match with the CIDR address of the network.
+              EOS
+            raise(Exception, msg_pb_address)
+          end
         end
-      else
-        return false
+        result = true
       end
 
+    else
+
+      # No 'network-name' key. We have to check the IP address.
+      if @ip_network_address.nil?
+        msg_no_addr = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+          |The interface `#{@conf.to_s}` has no `address` options and
+          |no `network-name` key, so it's impossible to test
+          |if this interface matches with a given network.
+          EOS
+        raise(Exception, msg_no_addr)
+      else
+        if @ip_network_address.eql?(network.get_ip_address)
+          result = true
+        end
+      end
+
+    end
+
+    return result
+
+  end
+
+
+  def get_matching_network(networks)
+
+    matching_networks = []
+
+    networks.each do |network|
+      if self.is_matching_network(network)
+        matching_networks.push(network)
+      end
+    end
+
+    n = matching_networks.length
+
+    if n > 1
+      names          = networks.map { |net| net.get_name }.join(', ')
+      matching_names = matching_networks.map { |net| net.get_name }.join(', ')
+      msg_too_net    = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+        |The interface `#{@conf.to_s}` has several matching
+        |networks. Among #{names}, these networks are
+        |matching with the interface: #{matching_names}.
+        EOS
+      raise(Exception, msg_too_net)
+    elsif n == 0
+      names      = networks.map { |net| net.get_name }.join(', ')
+      msg_no_net = <<-"EOS".gsub(/^\s*\|/, '').split("\n").join(' ')
+        |The interface `#{@conf.to_s}` has no matching
+        |network. Among #{names}, no network is matching
+        |with the interface.
+        EOS
+      raise(Exception, msg_no_net)
+    else
+      # There is only one matching network.
+      return matching_networks[0].get_name
     end
 
   end
 
 
+  def get_conf
+    @conf
+  end
+
+  def get_name
+    @name
+  end
+
+  def get_method
+    @method
+  end
+
+  def has_address_option
+    @has_address_option
+  end
+
+  def get_ip_address
+    @ip_address
+  end
+
+  def get_ip_network_address
+   @ip_network_address
+  end
+
 end
+
+
+
 
 class Network
 
@@ -197,6 +338,26 @@ class Network
       raise(Exception, msg_bad_address)
     end
 
+  end
+
+  def get_conf
+    @conf
+  end
+
+  def get_name
+    @name
+  end
+
+  def get_cidr_address
+    @cidr_address
+  end
+
+  def get_vlan_id
+    @vlan_id
+  end
+
+  def get_ip_address
+    @ip_address
   end
 
 end
--- puppet4-mod/network/lib/ruby/test.rb
+++ puppet4-mod/network/lib/ruby/test.rb
@@ -4,31 +4,34 @@ require 'json'
 require "#{script_dir}/interface"
 
 conf = {
+        #'network-name' => 'network-mgt',
         'name'    => 'eth0',
         'method'  => 'dhcp',
         'comment' => 'Blabla blabla',
         'options' => {
                        'key1'    => 'value1',
                        'key2'    => '1234',
-                       'address' => '172.31.10.11/20',
+                       'address' => '172.31.10.34',
+                       'netmask' => '255.255.0.0',
                       },
        }
 
 iface = Interface.new(conf)
 
-puts iface.instance_variable_get(:@name)
-puts iface.instance_variable_get(:@ip_address)
-puts JSON.pretty_generate(iface.instance_variable_get(:@conf))
+puts iface.get_name
+puts iface.get_ip_address
+puts JSON.pretty_generate(iface.get_conf)
 
 conf2 = {
          'name'         => 'network-mgt',
-         'cidr-address' => '172.31.0.0/20',
+         'cidr-address' => '172.31.10.0/24',
          'vlan-id'      => 1001,
          'dns-servers'  => [ 'a', 'b' ],
 }
 
 network = Network.new(conf2)
 
-puts JSON.pretty_generate(network.instance_variable_get(:@conf))
-puts network.instance_variable_defined?(:@ip_address).to_s
+puts JSON.pretty_generate(network.get_conf)
+
+puts "matching? " + iface.is_matching_network(network).to_s
 
