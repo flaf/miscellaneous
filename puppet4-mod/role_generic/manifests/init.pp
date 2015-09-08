5011ebb952037be4adc05937d9f8b4c7285ea10f
François Lafont <flafdivers@free.fr>
2015-09-09 01:12:34 +0200

    Wed Sep  9 01:12:34 CEST 2015: Update

--- puppet4-mod/role_generic/manifests/init.pp
+++ puppet4-mod/role_generic/manifests/init.pp
@@ -1,10 +1,10 @@
 class role_generic {
 
-  include '::basic_ssh::server'
-  include '::basic_ssh::client'
-  include '::keyboard'
-  include '::locale'
-  include '::timezone'
+  #include '::basic_ssh::server'
+  #include '::basic_ssh::client'
+  #include '::keyboard'
+  #include '::locale'
+  #include '::timezone'
   include '::network'
 
 }
