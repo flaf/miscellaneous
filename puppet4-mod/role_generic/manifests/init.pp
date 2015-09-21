cf70e9cc00fc2c55b5cd05a3bb9646e900dcd51b
lafont <francois.lafont@crdp.ac-versailles.fr>
2015-09-21 17:16:19 +0200

    Mon Sep 21 17:16:19 CEST 2015: Update

--- puppet4-mod/role_generic/manifests/init.pp
+++ puppet4-mod/role_generic/manifests/init.pp
@@ -2,13 +2,13 @@ class role_generic {
 
   include '::network'
   include '::network::resolv_conf'
+  include '::network::ntp'
   include '::repository::distrib'
   include '::basic_ssh::server'
   include '::basic_ssh::client'
   include '::keyboard'
   include '::locale'
   include '::timezone'
-  include '::ntp'
   include '::puppetagent'
   include '::mcollective::server'
 
