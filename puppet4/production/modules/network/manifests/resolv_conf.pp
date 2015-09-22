f1a13922c7105362aa1d76a90c9411acdc2455bf
François Lafont <flafdivers@free.fr>
2015-09-22 03:02:20 +0200

    Tue Sep 22 03:02:20 CEST 2015: Update

--- puppet4/production/modules/network/manifests/resolv_conf.pp
+++ puppet4/production/modules/network/manifests/resolv_conf.pp
@@ -19,22 +19,47 @@ class network::resolv_conf (
 
   ::homemade::is_supported_distrib($supported_distributions, $title)
 
-  # With "ensure => file" is the file is a symlink to
-  # "../run/resolvconf/resolv.conf", the symlink will
-  # be removed and replaced by a regular file. So,
-  # resolvconf mechanism will be disabled.
-  file { '/etc/resolv.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => epp('network/resolv.conf.epp',
-                   { 'domain'      => $domain,
-                     'search'      => $search,
-                     'nameservers' => $nameservers,
-                     'timeout'     => $timeout,
-                   }
-                  ),
+  $interfaces = ::network::data()['network::interfaces']
+
+  # Get the interfaces configured via DHCP.
+  # Be careful, with .filter if the first argument is
+  # a hash, the loop entry is an array with [key, value].
+  $dhcp_ifaces = $interfaces.filter |$iface| {
+    $dhcp_families = ['inet', 'inet6'].filter |$family| {
+      if $iface[1].has_key($family) and $iface[1][$family]['method'] == 'dhcp' {
+        true
+      } else {
+        false
+      }
+    }
+    !$dhcp_families.empty
+  }
+
+  # No DHCP, so we can manage the resolv.conf file.
+  # Indeed, if at least one interface is configured
+  # via DHCP, this file will be (and should be) updated
+  # via the DHCP mechanism, not via Puppet. It seems
+  # to me more logical.
+  if $dhcp_ifaces.empty {
+
+    # With "ensure => file" is the file is a symlink to
+    # "../run/resolvconf/resolv.conf", the symlink will
+    # be removed and replaced by a regular file. So,
+    # resolvconf mechanism will be disabled.
+    file { '/etc/resolv.conf':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => epp('network/resolv.conf.epp',
+                     { 'domain'      => $domain,
+                       'search'      => $search,
+                       'nameservers' => $nameservers,
+                       'timeout'     => $timeout,
+                     }
+                    ),
+    }
+
   }
 
 }
