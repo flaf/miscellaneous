0fe8e010edec774a9098a65c8a6a91ba3cea9c8a
François Lafont <flafdivers@free.fr>
2015-10-13 00:21:28 +0200

    Tue Oct 13 00:21:28 CEST 2015: Update

--- puppet/modules/unix_accounts/functions/data.pp
+++ puppet/modules/unix_accounts/functions/data.pp
@@ -1,7 +1,7 @@
 function unix_accounts::data {
 
   # Data lookup in hiera or in the environment.conf.
-  $users            = lookup('unix_accounts', Hash[String[1], Data, 1])
+  $users            = lookup('unix_accounts', Hash[String[1], Data, 1], 'hash')
   $ssh_public_keys  = lookup('ssh_public_keys', Hash[String[1], String[1], 1])
   $fqdn_in_prompt   = false
 
