2a07e14bc003f415ec50854f631ee9c4d8c726c7
François Lafont <flafdivers@free.fr>
2015-10-24 05:00:12 +0200

    Sat Oct 24 05:00:12 CEST 2015: Update

--- pxe/elea/preseed-mysql-elea-part-uefi.sh
+++ pxe/elea/preseed-mysql-elea-part-uefi.sh
@@ -72,7 +72,7 @@ done
 
 ### Partitioning on the non-SSD drives. ###
 n=0
-for i in a b
+for i in a
 do
     # a => n = 1, b => n = 2.
     n=$((n + 1))
@@ -83,69 +83,34 @@ do
     # to enable the Bios-UEFI.
     a=1
     b=$((250 + a)) # Size == 250MiB
-    $parted /dev/sd${i} unit MiB mkpart uefi${n}unused $a $b
-    part_num=$((part_num + 1))
-
-    # The biosgrub partitions.
-    a=$b
-    b=$((1 + a)) # Size == 1MiB
-    $parted /dev/sd${i} unit MiB mkpart biosgrub${n} $a $b
-    $parted /dev/sd${i} set $part_num bios_grub on
+    $parted /dev/sd${i} unit MiB mkpart uefi${n} $a $b
+    $parted /dev/sd${i} set $part_num esp on
     part_num=$((part_num + 1))
 
     # The root partitions (will be a RAID1 volume).
     a=$b
     b=$((30 * 1024 + a)) # Size == 30GiB
     $parted /dev/sd${i} unit MiB mkpart system${n} $a $b
-    $parted /dev/sd${i} set $part_num raid on
     part_num=$((part_num + 1))
 
     # The swap (will be a RAID1 volume).
     a=$b
     b=$((8 * 1024 + a)) # Size == 64GiB
     $parted /dev/sd${i} unit MiB mkpart swap${n} linux-swap $a $b
-    $parted /dev/sd${i} set $part_num raid on
     part_num=$((part_num + 1))
 
 done
 
 
-### Partitioning on the SSD drives. ###
-n=0
-for i in c d
-do
-    # c => n = 1, d => n = 2.
-    n=$((n + 1))
-
-    # The swap (will be a RAID1 volume).
-    a=1
-    b='-1cyl' # The last cylinder
-    $parted /dev/sd${i} -- unit MiB mkpart ssd${n} $a $b
-    $parted /dev/sd${i} set 1 raid on
-done
 
 
-### Creation of the RAID volumes. ###
 
-# For the system (/) partition.
-mdadm --create /dev/$raid1system --level=1 --raid-devices=2 /dev/sda3 /dev/sdb3 --force --run
-
-# For the swap partition.
-mdadm --create /dev/$raid1swap --level=1 --raid-devices=2 /dev/sda4 /dev/sdb4 --force --run
-
-# The SSD RAID1 volume.
-mdadm --create /dev/$raid1ssd --level=1 --raid-devices=2 /dev/sdc1 /dev/sdd1 --force --run
-
-
-### Creation of the volume group LVM on the SSD RAID1 volume. ###
-
-pvcreate -ff --yes /dev/$raid1ssd
-vgcreate --force --yes vg1 /dev/$raid1ssd
 
 
 ### Creation of the file systems. ###
 
-mkfs.ext4 -F -E lazy_itable_init=0 -L system /dev/$raid1system
-mkswap -L swap /dev/$raid1swap
+mkfs.vfat -F 32 -n 'uefi' /dev/sda1
+mkfs.ext4 -F -E lazy_itable_init=0 -L system /dev/sda2
+mkswap -L swap /dev/sda3
 
 
