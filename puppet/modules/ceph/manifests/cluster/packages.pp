40599f1a3dbb8ef54be57d756e9d5ece50500c5d
François Lafont <flafdivers@free.fr>
2015-11-06 22:06:41 +0100

    Fri Nov  6 22:06:41 CET 2015: Update

--- puppet/modules/ceph/manifests/cluster/packages.pp
+++ puppet/modules/ceph/manifests/cluster/packages.pp
@@ -2,13 +2,19 @@ class ceph::cluster::packages {
 
   require '::repository::ceph'
 
+  # Deprecated: in fact with this kernel I had curious
+  #             a behavior where the load average was
+  #             blocked to ~ 0.5 even when the server
+  #             was totally idle. Currenly, I prefer to
+  #             keep the default kernel 3.13.
+  #
   # With Trusty, it's better to use a 3.16 kernel.
   #
   # TODO: there is too the linux-image-generic-lts-vivid, ie a
   #       Linux kernel version 3.19. Maybe make some tests.
-  if $::lsbdistcodename == 'trusty' {
-    ensure_packages(['linux-image-generic-lts-utopic'], { ensure => present, })
-  }
+  #if $::lsbdistcodename == 'trusty' {
+  #  ensure_packages(['linux-image-generic-lts-utopic'], { ensure => present, })
+  #}
 
   $packages = [
                 'ceph',
