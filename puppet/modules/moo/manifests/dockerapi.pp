d38576af937c1d54b50e5e0e4b19579e3b6989d5
François Lafont <flafdivers@free.fr>
2016-01-05 11:32:58 +0100

    Tue Jan  5 11:32:58 CET 2016: Update

--- puppet/modules/moo/manifests/dockerapi.pp
+++ puppet/modules/moo/manifests/dockerapi.pp
@@ -2,10 +2,13 @@ class moo::dockerapi {
 
   ensure_packages( [ 'python-pip' ], { ensure => present } )
 
+  # With docker from Ubuntu Trusty repository, it's docker
+  # version 1.6.2 and the API is version 1.18. So we need
+  # to install docker-py version 1.2.3.
   exec { 'pip-install-docker-py':
     path    => '/usr/sbin:/usr/bin:/sbin:/bin',
     user    => 'root',
-    command => 'pip install docker-py',
+    command => 'pip install docker-py==1.2.3',
     unless  => "pip list | grep '^docker-py'",
   }
 
