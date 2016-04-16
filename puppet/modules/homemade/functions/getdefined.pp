ffdbf67f50e0ab4d67b8115ad0f82c96161e8abe
François Lafont <flafdivers@free.fr>
2016-04-16 09:28:16 +0200

    Sat Apr 16 09:28:16 CEST 2016: Update

---
+++ puppet/modules/homemade/functions/getdefined.pp
@@ -0,0 +1,19 @@
+function homemade::getdefined (
+  String[1] $var_name,
+  String[1] $class_name,
+) {
+
+  $value = getvar($var_name)
+
+  if $value =~ Undef {
+    @("END"/L).fail
+      ${class_name}: sorry the variable `${var_name}` is undefined \
+      which is forbidden by the function `getdefined()`.
+      |- END
+
+      $title
+  }
+
+}
+
+
