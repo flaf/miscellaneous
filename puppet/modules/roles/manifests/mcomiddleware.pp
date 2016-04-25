a41bcc53e85117cc6372b57f59b1c395f36d52c5
lafont <francois.lafont@ac-versailles.fr>
2016-04-25 23:29:20 +0200

    Mon Apr 25 23:29:20 CEST 2016: Update

--- puppet/modules/roles/manifests/mcomiddleware.pp
+++ puppet/modules/roles/manifests/mcomiddleware.pp
@@ -1,13 +1,13 @@
 class roles::mcomiddleware {
 
+  include '::roles::mcomiddleware::params'
+  include '::puppetagent::params'
+
   # This is a resource-like declaration, so it must be the first.
   class { '::mcomiddleware::params':
     puppet_ssl_dir => $::puppetagent::params::ssldir,
     exchanges      => $::roles::mcomiddleware::params::exchanges,
   }
-
-  include '::roles::mcomiddleware::params'
-  include '::puppetagent::params'
 
   # This present role include the role "generic".
   include '::roles::generic'
