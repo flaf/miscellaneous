b18e65d9f0db0a5878b091d4086805caeb56e7ba
François Lafont <flafdivers@free.fr>
2016-05-26 00:40:57 +0200

    Thu May 26 00:40:57 CEST 2016: Update

--- puppet/modules/ceph/types/keyring.pp
+++ puppet/modules/ceph/types/keyring.pp
@@ -3,6 +3,9 @@ type Ceph::Keyring = Struct[{
   'properties'             => Array[String[1], 1],
   Optional['radosgw_host'] => String[1],
   Optional['rgw_dns_name'] => String[1],
+  Optional['owner']        => String[1],
+  Optional['group']        => String[1],
+  Optional['mode']         => String[1],
 }]
 
 
