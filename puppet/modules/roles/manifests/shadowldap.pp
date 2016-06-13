13269ed5d9494a2df0c91606ec368bcbe5e7f0e2
lafont <francois.lafont@crdp.ac-versailles.fr>
2016-06-13 18:33:36 +0200

    Mon Jun 13 18:33:36 CEST 2016: Update

---
+++ puppet/modules/roles/manifests/shadowldap.pp
@@ -0,0 +1,30 @@
+class roles::shadowldap {
+
+  # TODO: create a basic module slapd, with an option to enable
+  #       ssl or not and with.
+  # TODO: in the module, manage a cron job to slapcat every day.
+  #       Add an option to set retention etc.
+  # TODO: management of a shinken check on _masterldap1_?
+
+  include '::roles::generic'
+  include '::keepalived_vip'
+
+  $packages = [ 'slapd', 'ldap-utils', 'git', 'openssl', 'ca-certificate' ]
+  ensure_packages( $packages, { ensure => present } )
+
+  file_line { 'edit-etc-default-slapd':
+    path    => '/etc/default/slapd',
+    line    => 'SLAPD_SERVICES="ldap:/// ldaps:/// ldapi:///"',
+    match   => '^[[:space:]]*SLAPD_SERVICES=.*$',
+    require => Package['slapd'],
+    notify  => Service['slapd'],
+  }
+
+  service { 'slapd':
+    ensure     => running,
+    hasrestart => true,
+  }
+
+}
+
+
