0c0f48acb84e0c1e0ea1578b63652e9e62cae982
François Lafont <flafdivers@free.fr>
2017-03-19 05:22:09 +0100

    Sun Mar 19 05:22:09 CET 2017: Update

--- puppet/modules/eximnullclient/functions/data.pp
+++ puppet/modules/eximnullclient/functions/data.pp
@@ -2,13 +2,19 @@ function eximnullclient::data {
 
   $dc_smarthost = $::facts.dig('networking', 'domain').then |$domain| {
     [ { 'address' => "smtp.${domain}", 'port' => 25 } ]
-  };
+  }
+
+  $supported_distributions = [
+                               'trusty',
+                               'xenial',
+                               'jessie'
+                             ];
 
   {
     eximnullclient::params::dc_smarthost            => $dc_smarthost,
     eximnullclient::params::redirect_local_mails    => '',
     eximnullclient::params::prune_from              => true,
-    eximnullclient::params::supported_distributions => ['trusty', 'jessie'],
+    eximnullclient::params::supported_distributions => $supported_distributions,
   }
 
 }
