d62baabb8cbf6106eab8bbcac6e02107b5ee8882
François Lafont <flafdivers@free.fr>
2017-03-19 03:00:53 +0100

    Use multi-assignment from class reference

--- puppet/modules/raid/manifests/init.pp
+++ puppet/modules/raid/manifests/init.pp
@@ -2,8 +2,10 @@ class raid {
 
   include '::raid::params'
 
-  $raid_controllers    = $::raid::params::raid_controllers
-  $classes2controllers = $::raid::params::classes2controllers
+  [
+    $raid_controllers,
+    $classes2controllers,
+  ] = Class['::raid::params']
 
   $raid_controllers.each |$raid_controller| {
     $classes2controllers.each |String[1] $this_class, Array[String[1], 1] $controllers_for_this_class | {
