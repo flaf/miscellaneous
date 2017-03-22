6d0c19761432f02cf068f3c2dee45441379ec577
François Lafont <flafdivers@free.fr>
2017-03-22 18:44:33 +0100

    Wed Mar 22 18:44:32 CET 2017: Update

--- puppet/modules/puppetserver/manifests/get_reports.pp
+++ puppet/modules/puppetserver/manifests/get_reports.pp
@@ -2,24 +2,8 @@ class puppetserver::get_reports (
   String[1] $file = '/root/reports',
 ) {
 
-  $query = 'reports[certname, receive_time, logs, metrics, noop] {order by certname, receive_time desc}'
-  $array = puppetdb_query($query)
-
-  # We remove reports with the same node (ie same certname).
-  $reports = $array.reduce([]) |$memo, $entry| {
-
-    $certname          = $entry['certname']
-    $previous_certname = case $memo[-1] =~ Undef {
-      true:    { ''                    }
-      default: { $memo[-1]['certname'] }
-    }
-
-    case $certname == $previous_certname  {
-      true:    { $memo              }
-      default: { $memo + [ $entry ] }
-    }
-
-  }
+  $query   = 'reports[certname, receive_time, logs, metrics, noop] {latest_report? = true order by certname}'
+  $reports = puppetdb_query($query)
 
   file { $file:
     ensure  => present,
