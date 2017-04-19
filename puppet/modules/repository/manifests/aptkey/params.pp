cf614c189335492619647ce5dc3f48b81b54e962
François Lafont <flafdivers@free.fr>
2017-04-19 05:25:52 +0200

    Wed Apr 19 05:25:52 CEST 2017: Update

--- puppet/modules/repository/manifests/aptkey/params.pp
+++ puppet/modules/repository/manifests/aptkey/params.pp
@@ -1,6 +1,6 @@
 class repository::aptkey::params (
   Optional[String[1]] $http_proxy,
-  String[1]           $keyserver,
+  Optional[String[1]] $keyserver,
   Array[String[1], 1] $supported_distributions,
 ) {
 }
