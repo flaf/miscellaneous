a332785449d35a83568a2e0a4e7e9f44f5b6db15
François Lafont <flafdivers@free.fr>
2017-04-23 13:30:26 +0200

    Sun Apr 23 13:30:26 CEST 2017: Update

---
+++ puppet/modules/httpproxy/README.md
@@ -0,0 +1,27 @@
+TODO: Please, make a real README file...
+
+# Module description
+
+Module to manage a HTTP proxy.
+
+
+
+
+# Usage
+
+Here is an example:
+
+```puppet
+class { '::httpproxy':
+  # ...
+}
+```
+
+
+
+
+# Parameters
+
+TODO...
+
+
---
+++ puppet/modules/httpproxy/functions/data.pp
@@ -0,0 +1,14 @@
+function httpproxy::data (
+  Hash                  $options,
+  Puppet::LookupContext $context,
+) {
+
+  #$supported_distributions = [ ... ];
+
+  {
+    #httpproxy::supported_distribution => $supported_distribution,
+  }
+
+}
+
+
