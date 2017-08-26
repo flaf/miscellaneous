ce16c5d8c88f1d7d25af580dbd82abbff0a4deab
François Lafont <flafdivers@free.fr>
2017-08-26 04:49:04 +0200

    Sat Aug 26 04:49:04 CEST 2017: Update

--- puppet/modules/roles/manifests/gitlab.pp
+++ puppet/modules/roles/manifests/gitlab.pp
@@ -21,6 +21,26 @@ class roles::gitlab {
     backup_cron_wrapper => ::roles::wrap_cron_mon($cron_backup_name),
   }
 
+  # For monitoring.
+  $external_url      = $::gitlab::params::external_url
+  $external_fqdn     = $external_url.regsubst(Regexp.new('^https?://'), '')
+  $sign_in_regex     = $::gitlab::params::sign_in_regex
+  $health_page_token = $::gitlab::params::health_page_token
+
+  if $sign_in_regex =~ Undef {
+    @("END"/L$).fail
+      ${title}: sorry you must define the parameter \
+      `gitlab::params::sign_in_regex` for this role.
+      |- END
+  }
+
+  if $health_page_token =~ Undef {
+    @("END"/L$).fail
+      ${title}: sorry you must define the parameter \
+      `gitlab::params::health_page_token` for this role.
+      |- END
+  }
+
   class { '::rsyncd::params':
     # The rsync user "gitlab" and its password must be defined
     # in the hiera configuration.
@@ -48,15 +68,41 @@ class roles::gitlab {
     "${fqdn} from ${title}"
   }
 
+  $custom_variables = [
+    {
+      'varname' => '_crons',
+      'value'   => {"cron-${cron_backup_name}" => [$cron_backup_name, '1d']},
+      'comment' => ["There is a daily Gitlab backup (${cron_backup_name})."],
+    },
+    {
+      'varname' => '_https_pages',
+      'value'   => {
+        'https-sign-page' => ["${external_fqdn}/users/sign_in", $sign_in_regex],
+        'health-page'     => ["${external_fqdn}/health_check?token=${health_page_token}", '^success$$'],
+      },
+    },
+  ]
+
+  $extra_info = case $::facts['networking']['fqdn'] == $external_fqdn {
+    false: {
+      {
+        'check_dns' => {
+          "DNS-${external_fqdn}" => {
+            'fqdn'             => $external_fqdn,
+            'expected-address' => '$HOSTADDRESS$'
+          },
+        }
+      }
+    }
+    default: {
+      undef
+    }
+  }
+
   monitoring::host::checkpoint {$gitlab_checkpoint_title:
     templates        => ['linux_tpl', 'https_tpl'],
-    custom_variables => [
-      {
-        'varname' => '_crons',
-        'value'   => {"cron-${cron_backup_name}" => [$cron_backup_name, '1d']},
-        'comment' => ["There is a daily Gitlab backup (${cron_backup_name})."],
-      }
-    ],
+    custom_variables => $custom_variables,
+    extra_info       => $extra_info,
   }
 
 }
