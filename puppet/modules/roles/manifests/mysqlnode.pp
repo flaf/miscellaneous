d7c1d558142ab3514a798b800e14ef60ee1a724d
François Lafont <flafdivers@free.fr>
2017-08-30 03:06:17 +0200

    Wed Aug 30 03:06:17 CEST 2017: Update

--- puppet/modules/roles/manifests/mysqlnode.pp
+++ puppet/modules/roles/manifests/mysqlnode.pp
@@ -20,6 +20,63 @@ class roles::mysqlnode {
 
   include '::keepalived_vip'
 
+
+  # Add the checkpoint.
+  $hostname      = $::facts['networking']['hostname']
+  $regex_eleapoc = Regexp.new('-eleapoc$')
+
+  if $hostname =~ $regex_eleapoc {
+    return()
+  }
+
+  $custom_variables = if ::roles::is_number_one() {
+    # In this role, we assume that the first mysql server
+    # must has the VIP. We take the first VIP only (and we
+    # hope the only VIP).
+    $vrrp_instances       = $::keepalived_vip::params::vrrp_instances
+    $first_vrrp_instance  = $vrrp_instances.keys.dig(0)
+    $vip                  = $vrrp_instances
+                              .dig($first_vrrp_instance, 'virtual_ipaddress', 0)
+                              .lest || {
+      @("END"/L$).fail
+        ${title}: sorry, no VIP address has been found in the module \
+        `keepalived_vip` and it is required in this role.
+        |- END
+    };
+    [
+      {'varname' => '_has_ip', 'value' => {'virtual-ip' => [$vip]}},
+    ]
+  } else {
+    undef
+  }
+
+  monitoring::host::checkpoint {"${fqdn} from ${title}":
+    templates        => ['linux_tpl', 'mysql-repl_tpl'],
+    custom_variables => $custom_variables,
+  }
+
+  # In this role, it will be moosql01 which define the
+  # checkpoint for the VIP host.
+  if $hostname == 'moosql01' {
+
+    $domain   = $::facts['networking']['domain']
+    $vip_fqdn = "moosql-vip.${domain}"
+
+    monitoring::host::checkpoint {"${vip_fqdn} from ${title}":
+      host_name        => $vip_fqdn,
+      address          => $vip,
+      templates        => ['generic-host_tpl*', 'dns_tpl'],
+      monitored        => true,
+      custom_variables => [
+        {
+          'varname' => '_resolvconf_dns_lookups',
+          'value'   => {"DNS-${vip_fqdn}" => [$vip_fqdn, "-a ${vip}"]},
+        },
+      ],
+    }
+
+  }
+
 }
 
 
